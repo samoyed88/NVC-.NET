554c881f2bcff4127a4e983a73ff9d2dd0bd96bf
samoyed88 <12130383@me.mcu.edu.tw>
2025-02-24 14:44:49 +0800

    MR2 專案完成

--- MV1/MV1/obj/Debug/net8.0/MV1.AssemblyInfo.cs
+++ MV1/MV1/obj/Debug/net8.0/MV1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MV1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3ac98c496a372a25b4c2951b0776f0f855040f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MV1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MV1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
